a5cc069ff7735ea083cc1740c9956b50c043c423
Tyana <tyana.stone@gmail.com>
2018-10-24 12:14:21 -0400

    finished adding heroku

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+console.log('Connected to MongoDB')
+
+})
+
+
+module.exports = mongoose
